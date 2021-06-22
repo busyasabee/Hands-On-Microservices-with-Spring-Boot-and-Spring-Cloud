6c95e7f13636acb545b431e273374fe2bc688a78
rider <dmitr.romashov@gmail.com>
2021-06-22 17:54:36 +0300

    Fix gradle and doc

--- Chapter17/microservices/product-composite-service/src/main/java/se/magnus/microservices/composite/product/ProductCompositeServiceApplication.java
+++ Chapter17/microservices/product-composite-service/src/main/java/se/magnus/microservices/composite/product/ProductCompositeServiceApplication.java
@@ -8,18 +8,18 @@ import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.reactive.function.client.WebClient;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2WebFlux;
+//import springfox.documentation.builders.PathSelectors;
+//import springfox.documentation.service.ApiInfo;
+//import springfox.documentation.service.Contact;
+//import springfox.documentation.spring.web.plugins.Docket;
+//import springfox.documentation.swagger2.annotations.EnableSwagger2WebFlux;
+//
+//import static java.util.Collections.emptyList;
+//import static org.springframework.web.bind.annotation.RequestMethod.*;
+//import static springfox.documentation.builders.RequestHandlerSelectors.basePackage;
+//import static springfox.documentation.spi.DocumentationType.SWAGGER_2;
 
-import static java.util.Collections.emptyList;
-import static org.springframework.web.bind.annotation.RequestMethod.*;
-import static springfox.documentation.builders.RequestHandlerSelectors.basePackage;
-import static springfox.documentation.spi.DocumentationType.SWAGGER_2;
-
-@EnableSwagger2WebFlux
+//@EnableSwagger2WebFlux
 @SpringBootApplication
 @ComponentScan("se.magnus")
 public class ProductCompositeServiceApplication {
@@ -39,28 +39,28 @@ public class ProductCompositeServiceApplication {
 	 *
 	 * @return
 	 */
-	@Bean
-	public Docket apiDocumentation() {
-
-		return new Docket(SWAGGER_2)
-			.select()
-			.apis(basePackage("se.magnus.microservices.composite.product"))
-			.paths(PathSelectors.any())
-			.build()
-				.globalResponseMessage(POST, emptyList())
-				.globalResponseMessage(GET, emptyList())
-				.globalResponseMessage(DELETE, emptyList())
-				.apiInfo(new ApiInfo(
-                    apiTitle,
-                    apiDescription,
-                    apiVersion,
-                    apiTermsOfServiceUrl,
-                    new Contact(apiContactName, apiContactUrl, apiContactEmail),
-                    apiLicense,
-                    apiLicenseUrl,
-                    emptyList()
-                ));
-    }
+//	@Bean
+//	public Docket apiDocumentation() {
+//
+//		return new Docket(SWAGGER_2)
+//			.select()
+//			.apis(basePackage("se.magnus.microservices.composite.product"))
+//			.paths(PathSelectors.any())
+//			.build()
+//				.globalResponseMessage(POST, emptyList())
+//				.globalResponseMessage(GET, emptyList())
+//				.globalResponseMessage(DELETE, emptyList())
+//				.apiInfo(new ApiInfo(
+//                    apiTitle,
+//                    apiDescription,
+//                    apiVersion,
+//                    apiTermsOfServiceUrl,
+//                    new Contact(apiContactName, apiContactUrl, apiContactEmail),
+//                    apiLicense,
+//                    apiLicenseUrl,
+//                    emptyList()
+//                ));
+//    }
 
 	@Bean
 	@LoadBalanced
